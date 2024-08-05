24424554d49b9e95a4254180771960d28691fbe1
TommyJae89 <85933259+TommyJae89@users.noreply.github.com>
2024-08-04 20:31:18 -0500

    Delete maintenance-task-tracker directory

--- maintenance-task-tracker/src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'; // Global CSS file
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Font Awesome
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
